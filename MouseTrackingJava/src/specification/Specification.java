805bff8a993482e20fe7fc8180ff2642e6a3729b
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2018-11-22 16:02:21 +0000

    Added json

--- MouseTrackingJava/src/specification/Specification.java
+++ MouseTrackingJava/src/specification/Specification.java
@@ -1,12 +1,40 @@
 package specification;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Specification {
 	
-	public static void makeUIFromSpecification(String jsonString)
+	public static BufferedImage makeUIFromSpecification(String jsonString)
 	{
 		JSONObject jObject = new JSONObject(jsonString);
+		String formName = jObject.getString("formName");
+		String formId = jObject.getString("formId");
+		int sizeH = jObject.getInt("sizeH");
+		int sizeW = jObject.getInt("sizeW");
+		BufferedImage renderedUi = new BufferedImage(sizeW, sizeH, BufferedImage.TYPE_3BYTE_BGR);
+		
+		for(int i = 0; i < sizeW; i++)
+			for(int j = 0; j < sizeH; j++)	
+				renderedUi.setRGB(j, i, Color.LIGHT_GRAY.getRGB());
+		
+		JSONArray jsonUiArray = jObject.getJSONArray("ui");
+		
+		for(int i = 0; i < jsonUiArray.length(); i++)
+		{
+			JSONObject UIObject = jsonUiArray.getJSONObject(i);
+			
+			String type = UIObject.getString("type");
+			String loc = UIObject.getString("loc");
+			String label = UIObject.getString("label");
+
+			
+		}
+		
+		return renderedUi;
 	}
 
 }
