8d4c7a7995356a6a04b214f1e8f763374c0506cf
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2018-12-04 10:57:16 +0000

    Updates

--- aritra/MouseTrackCode/Capture scripts/markCursor.py
+++ aritra/MouseTrackCode/Capture scripts/markCursor.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import glob
+import time
 
 templ = cv2.imread('cursor1.jpg', cv2.IMREAD_GRAYSCALE)
 mask = cv2.imread('mask3.jpg', cv2.IMREAD_GRAYSCALE)
@@ -15,13 +16,18 @@ cv2.moveWindow('Output', 0, 0)
 
 for filepath in glob.iglob('tl/*1.jpg'):
 	img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-
+	
+	startTime = time.time()
 	method_accepts_mask = (cv2.TM_SQDIFF == match_method or match_method == cv2.TM_CCORR_NORMED)
 	if (use_mask and method_accepts_mask):
 		result = cv2.matchTemplate(img, templ, match_method, None, mask)
 	else:
 		result = cv2.matchTemplate(img, templ, match_method)
-
+	
+	#minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result, None)
+	#print(minVal)
+	#print(maxVal)
+	
 	cv2.normalize( result, result, 0, 1, cv2.NORM_MINMAX, -1)
 	minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result, None)
 
@@ -38,6 +44,8 @@ for filepath in glob.iglob('tl/*1.jpg'):
 
 	# for pt in zip(*loc[::-1]):
 		# cv2.rectangle(img, pt, (pt[0] + h, pt[1] + w), (0,255,255), 2, 8, 0)
+	
+	print(time.time() - startTime)
 	cv2.rectangle(img, matchLoc, (matchLoc[0] + 12, matchLoc[1] + 20), (0,255,255), 2, 8, 0 )
 
 	cv2.imshow('Output',img)
