fbb2a862d0caded3ce8fca4b71de6fe58788e797
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2018-12-21 16:08:35 +0000

    refactor

--- aritra/MouseTrackCode/MouseTrackingJava/src/app/ENV.java
+++ aritra/MouseTrackCode/MouseTrackingJava/src/app/ENV.java
@@ -7,7 +7,7 @@ public class ENV {
 	public static final int SCREEN_X = 1919;
 	public static final int SCREEN_Y = 1079;
 	
-	public static final String DEVICE = "/dev/ttyUSB1";
+	public static final String DEVICE = "/dev/ttyUSB0";
 	
 	public static final char[] intSend = new char[2];
 	public static final char[] pkSend = new char[2];
